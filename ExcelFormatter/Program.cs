23bd0862ec81afd76f162a273f0767913eb35769
nekomimi-daimao <35223467+nekomimi-daimao@users.noreply.github.com>
2022-01-18 07:51:10 +0900

    format excel

--- ExcelFormatter/Program.cs
+++ ExcelFormatter/Program.cs
@@ -1,2 +1,99 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using ClosedXML.Excel;
+using Kurukuru;
+
+const string extensionExcel = ".xlsx";
+
+
+// entry
+
+Console.OutputEncoding = Encoding.UTF8;
+
+var argPath = args.FirstOrDefault();
+if (string.IsNullOrEmpty(argPath))
+{
+    Console.WriteLine("enter excel file or directory");
+    return;
+}
+
+var excelFiles = SearchExcelFiles(argPath);
+var fileCount = excelFiles.Length;
+
+if (fileCount == 0)
+{
+    Console.WriteLine($"no excel file {argPath}");
+    return;
+}
+
+using var spinnerTotal = new Spinner($"{argPath}");
+var failed = new List<FileInfo>();
+
+for (var count = 0; count < excelFiles.Length; count++)
+{
+    spinnerTotal.Text = $"{argPath} {count} / {fileCount}";
+
+    var file = excelFiles[count];
+    using var spinner = new Spinner(file.Name, Patterns.Arc);
+    try
+    {
+        Format(file);
+        spinner.Succeed(file.Name);
+    }
+    catch (Exception e)
+    {
+        spinner.Fail(e?.Message);
+        failed.Add(file);
+    }
+}
+
+if (failed.Count == 0)
+{
+    spinnerTotal.Succeed();
+}
+else
+{
+    spinnerTotal.Fail($"{argPath} {failed.Count} / {fileCount}");
+    foreach (var fileInfo in failed)
+    {
+        Console.WriteLine(fileInfo.Name);
+    }
+}
+
+
+// functions
+
+FileInfo[] SearchExcelFiles(string? path)
+{
+    if (string.IsNullOrEmpty(path))
+    {
+        return Array.Empty<FileInfo>();
+    }
+
+    if (path.EndsWith(extensionExcel))
+    {
+        var fileInfo = new FileInfo(path);
+        return fileInfo.Exists ? new[] { fileInfo, } : Array.Empty<FileInfo>();
+    }
+
+    var dirInfo = new DirectoryInfo(path);
+    if (!dirInfo.Exists)
+    {
+        return Array.Empty<FileInfo>();
+    }
+
+    return dirInfo.EnumerateFiles($"*{extensionExcel}")
+        .ToArray();
+}
+
+void Format(FileSystemInfo info)
+{
+    using var book = new XLWorkbook(info.FullName);
+    book.Author = null;
+    foreach (var worksheet in book.Worksheets)
+    {
+        worksheet.Author = null;
+        worksheet.Cell(1, 1).SetActive();
+        worksheet.SheetView.ZoomScale = 100;
+    }
+    book.Save();
+}
